1c18d0edced3e757607a2b0478e9b1fa7de721e5
N.Kamala Santhoshini <nidamarthikamalasanthoshini@gmail.com>
2026-04-01 14:05:22 +0530

    Update API base URL to include HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "ecommerce-backend-production-b7d8.up.railway.app/api"
+  baseURL: "https://ecommerce-backend-production-b7d8.up.railway.app/api"
 });
 
 export default API;
